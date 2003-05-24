0ae02339d1b191e10a98ee6bcfd58d4e1b3c5871
William McBrine <wmcbrine@gmail.com>
2003-05-24 03:54:55 +0000

    More copyright/address updates.

--- mmail/bw.cc
+++ mmail/bw.cc
@@ -3,7 +3,7 @@
  * Blue Wave class
 
  Copyright (c) 1996 Toth Istvan <stoty@vma.bme.hu>
- Copyright (c) 2001 William McBrine <wmcbrine@users.sourceforge.net>
+ Copyright (c) 2003 William McBrine <wmcbrine@users.sf.net>
 
  Distributed under the GNU General Public License.
  For details, see the file COPYING in the parent directory. */
